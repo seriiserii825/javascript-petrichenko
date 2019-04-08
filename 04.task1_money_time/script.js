55e9817a15c25326f4ce2688bca162b265fb4791
serii <seriiburduja@gmail.com>
2019-04-08 20:37:27 +0300

    finish task 1

--- 04.task1_money_time/script.js
+++ 04.task1_money_time/script.js
@@ -1,20 +1,50 @@
 'use strict';
 
-let money = prompt('Ваш бюджет на месяц?', '');
-let time = prompt("Введите дату в формате YYYY-MM-DD", '');
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
+let a;
+let b;
+
+let time = prompt("Введите дату в формате YYYY-MM-DD", '');
+let	money = +prompt("Ваш бюджет на месяц?", 'Число');
+
+while ( money == undefined || money == null || money == '' || !isNumeric(money) ) { //тут проверка работает
+	money = prompt("Ваш бюджет на месяц?", '');
+}
+
+while ( time == undefined || time == null || time == '' ) { //тут проверка работает
+	time = prompt("Введите дату в формате YYYY-MM-DD", '');
+}
 
 let appData = {
-	money: money,
-	time: time,
+	budget: money,
+	timeData: time,
 	expenses: {},
 	optionalExpenses: {},
 	income: [],
 	savings: false,
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", '');
-let a2 = prompt("Во сколько обойдется?", "");
-let a3 = prompt("Введите обязательную статью расходов в этом месяце", '');
-let a4 = prompt("Во сколько обойдется?", "");
+appData.budget = money;
+appData.timeData = time;
+
+for(let i = 0; i < 2; i++){
+	a = +prompt("Введите обязательную статью расходов в этом месяце", 'Число');
+	b = +prompt("Во сколько обойдется?", 'Число');
+
+	while ( a == undefined || a == null || a == '' || !isNumeric(a) ) { //тут проверка работает
+		a = +prompt("Введите обязательную статью расходов в этом месяце", 'Число');
+	}
+
+	while ( b == undefined || b == null || b == '' || !isNumeric(b) ) { //тут проверка работает
+		b = +prompt("Во сколько обойдется?", 'Число');
+	}
+
+	appData.expenses[a] = b;
+
+}
+
+
 
